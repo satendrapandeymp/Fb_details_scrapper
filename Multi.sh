6269179cd01bdb056c143732822d9cb29676cbc9
Satyendra Pandey <satyendrapandeyiitk@gmail.com>
2018-04-10 17:13:43 +0530

    Minor changes

--- Multi.sh
+++ Multi.sh
@@ -1,12 +1,9 @@
-
-source ~/Documents/testing/venv/bin/activate
-
 for i in satendrapandeymp narendra.pandey.9480 # Put name of peoples sepated by space
 do
-	mkdir "$i"
-	cp /home/intel/Documents/testing/Fb_details_scrapper/Web-Scraping.py /home/intel/Desktop/Test/$i
+	mkdir "$i"	
+	cp Web-Scraping.py $i
 	cd $i
-	python Web-Scraping.py #Your_Username #Your_Password $i &
+	python Web-Scraping.py #Your username #Your pass $i > log_$i.txt &
 	cd ..
 	echo $i
 done
